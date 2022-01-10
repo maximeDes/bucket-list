a514e1423928d389e59a10134a02ebeba2db3dc2
maximeDes <maxime.descatoire2021@campus-eni.fr>
2022-01-10 15:56:29 +0100

    modif tp

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,14 +11,8 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      * */
-    public function home(){
+    public function home():Response{
        return $this-> render('main/home.html.twig');
 
-    }
-    /**
-     * @Route("/test", name="main_test")
-     * */
-    public function test(){
-        return $this-> render('main/test.html.twig');
     }
 }
