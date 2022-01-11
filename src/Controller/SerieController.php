66bdffe1ab1d42fe862ea183c45111bdfb65bafa
maximeDes <maxime.descatoire2021@campus-eni.fr>
2022-01-11 14:13:34 +0100

    demo 1 module 4 ( les séries )

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/series", name="serie_")
+ *
+ */
+class SerieController extends AbstractController
+{
+    /**
+     * @Route("", name="list")
+     */
+    public function list(): Response
+    {
+        //todo: allez chercher les séries en BDD
+        return $this->render('serie/list.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(int $id): Response
+    {
+        //todo: allez chercher la séries en BDD
+        return $this->render('serie/details.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(): Response
+    {
+        //todo: créer une séries a ajouté en BDD
+        return $this->render('serie/create.html.twig', [
+
+        ]);
+    }
+}
